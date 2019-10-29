4291ca81831030ff17b0865696c2830b85eefcbe
Server-InvidA <57058961+Server-InvidA@users.noreply.github.com>
2019-10-29 10:17:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ bot.on('ready', function () {
 })
 
 bot.on('message', function (message) {
-	const autor = message.autor
+	const autor = message.autor()
 	message.channel.send(autor)
 	if (message.content === '?create') {
 		console.send("test")
